061d8f4567c5a882e5f55c9f3cdd0ddcbf418256
Eric Mugo <mugoericpg20@gmail.com>
2023-06-13 13:53:30 +0300

    done

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,22 @@
 class Role < ActiveRecord:Base
+    has_many :auditions
+
+    def actors
+      auditions.pluck(:actor)
+    end
+  
+    def locations
+      auditions.pluck(:location)
+    end
+  
+    def lead
+      hired_audition = auditions.find_by(hired: true)
+      hired_audition || 'no actor has been hired for this role'
+    end
+  
+    def understudy
+      hired_auditions = auditions.where(hired: true)
+      hired_auditions[1] || 'no actor has been hired for understudy for this role'
+    end
+
 end
