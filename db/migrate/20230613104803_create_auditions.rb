471ed188fc2c8d9825f5c7126f345fadf7abf072
Eric Mugo <mugoericpg20@gmail.com>
2023-06-13 13:52:52 +0300

    role

--- db/migrate/20230613104803_create_auditions.rb
+++ db/migrate/20230613104803_create_auditions.rb
@@ -1,5 +1,11 @@
 class CreateAuditions < ActiveRecord::Migration[5.2]
-  def change
-    create_table :auditions do |t|
+  create_table :auditions do |t|
+    t.string :actor
+    t.string :location
+    t.boolean :hired, default: false
+    t.references :role, foreign_key: true
+
+    t.timestamps
+   end
   end
 end
